1cc4963262fe542af3a577247e2f33449fcfde86
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-02-23 22:18:23 -0400

    Add Routes script

---
+++ Routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Routes = () => (
+    <BrowserRouter>
+    {/*Navbar*/}
+    <Switch>
+        <Route path="/" exact>
+            {/*Home*/}
+        </Route>
+        <Route path="/search">
+            {/*Search*/}
+        </Route>
+    </Switch>
+    </BrowserRouter>
+)
+
+export default Routes;
